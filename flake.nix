9429594ac9fbd8811f368a6889669f93e8609d6d
Sewen Thy <me@sewenthy.dev>
2025-08-27 14:04:50 +0800

    flake: init

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = [ "x86_64-linux" ];
+      perSystem = { pkgs, ... }:
+        ############################ PACKAGES ############################
+        let
+          ocamlPackages = pkgs.ocaml-ng.ocamlPackages_5_3;
+          extunix = ocamlPackages.buildDunePackage {
+            pname = "extunix";
+            version = "20250621.0";
+            minimumOCamlVersion = "4.13";
+            src = pkgs.fetchFromGitHub {
+              owner = "dombong";
+              repo = "extunix";
+              rev = "5e10dd70b30b6d6180b63e33c463858c2d1591ae";
+              sha256 = "sha256-0phYdjKLR6qXR03Lf8moqQPn/Wwdryo4p6d0AUZpkbE=";
+            };
+            nativeBuildInputs = with ocamlPackages; [ findlib ocaml ];
+
+            buildInputs = with ocamlPackages; [
+              ocaml
+              findlib
+              ppxlib
+              dune-configurator
+            ];
+          };
+        in {
+          packages.default = extunix;
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ extunix ];
+          };
+        };
+    };
+}
